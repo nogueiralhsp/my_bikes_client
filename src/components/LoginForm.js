9801cc3931f34b6aed609b3dd7566dac5ce91d87
Luiz Henrique Pinto <nogueiralhsp@gmail.com>
2023-05-30 05:10:08 +0100

    formated LoginForm component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,33 +1,61 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('sending info to login');
-    // code to handle form submission
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-      <p>
-        Don't have an account? <a href="/signup">Sign up</a>
-      </p>
-    </form>
-  );
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const user = JSON.stringify(this.state)
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: user,
+      redirect: 'follow'
+    };
+
+    fetch(`${process.env.REACT_APP_API_URL}/auth/login`, requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <label>
+          Username:
+          <input
+            type="text"
+            // value={username}
+            onChange={(event) => this.setState({ username: event.target.value })} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            // value={password}
+            onChange={(event) => this.setState({ password: event.target.value })} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+        <p>
+          Don't have an account? <a href="/signup">Sign up</a>
+        </p>
+      </form>
+    );
+  }
 }
 
 export default LoginForm;
